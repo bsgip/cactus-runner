367e87a252a3cf40217bae7b0de6c05c0ab5080e
LachlanJW <watsonjlachlan@gmail.com>
2025-10-30 15:24:29 +1100

    WIP

--- src/cactus_runner/app/main.py
+++ src/cactus_runner/app/main.py
@@ -144,8 +144,8 @@ def create_app() -> web.Application:
     app.router.add_route("POST", mount + "finalize", handler.finalize_handler)
 
     # For retrieving request logs
-    app.router.add_route("GET", MOUNT_POINT + "request/{request_id}", handler.get_request_raw_data_handler)
-    app.router.add_route("GET", MOUNT_POINT + "requests", handler.list_request_ids_handler)
+    app.router.add_route("GET", mount + "request/{request_id}", handler.get_request_raw_data_handler)
+    app.router.add_route("GET", mount + "requests", handler.list_request_ids_handler)
 
     # Add catch-all route for proxying all other requests to CSIP-AUS reference server
     app.router.add_route("*", mount + "{proxyPath:.*}", handler.proxied_request_handler)
