51c1438668a36e901732812c3e7e3a842a97d0a0
Mike Turner <mike.turner@anu.edu.au>
2026-02-11 09:18:08 +0800

    Replace multiple asserts with assert_class_instance_equality

--- tests/unit/app/test_models.py
+++ tests/unit/app/test_models.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
+from assertical.asserts.generator import assert_class_instance_equality
 from assertical.fake.generator import generate_class_instance
 from envoy.server.model import SiteReadingType
 from envoy.server.model.site import Site as EnvoySite
-from envoy_schema.server.schema.sep2.types import UomType
 
 from cactus_runner.models import ReadingType, Site, StepInfo, StepStatus
 
@@ -25,12 +25,7 @@ def test_reading_type_from_site_reading_type():
 
     reading_type = ReadingType.from_site_reading_type(site_reading_type)
 
-    assert reading_type.site_reading_type_id == site_reading_type.site_reading_type_id
-    assert reading_type.group_mrid == site_reading_type.group_mrid
-    assert reading_type.uom == site_reading_type.uom
-    assert isinstance(reading_type.uom, UomType)
-    assert reading_type.created_time == site_reading_type.created_time
-    assert isinstance(reading_type.created_time, datetime)
+    assert_class_instance_equality(ReadingType, reading_type, site_reading_type)
 
 
 def test_reading_type_serialisation():
