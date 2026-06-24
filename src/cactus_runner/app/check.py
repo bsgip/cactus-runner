ce0c13449a054d606ab8571789f17a8f1a2e3dc7
Lachlan <161661313+LachlanJW@users.noreply.github.com>
2026-06-24 14:53:54 +1000

    Envoy v1.5.0 (#192)
    * version
    
    * crud wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * tests
    
    * tests
    
    * Merge fixes
    
    * Test Fixes
    
    ---------
    
    Co-authored-by: Josh Vote <jjvote@gmail.com>

--- src/cactus_runner/app/check.py
+++ src/cactus_runner/app/check.py
@@ -19,7 +19,6 @@ from envoy.server.model.archive.doe import ArchiveDynamicOperatingEnvelope
 from envoy.server.model.doe import DynamicOperatingEnvelope
 from envoy.server.model.response import DynamicOperatingEnvelopeResponse
 from envoy.server.model.site import (
-    SiteDER,
     SiteDERRating,
     SiteDERSetting,
     SiteDERStatus,
@@ -399,9 +398,7 @@ async def check_der_settings_contents(  # noqa: C901
     if site is None:
         return CheckResult(False, "No EndDevice is currently registered.")
 
-    response = await session.execute(
-        select(SiteDERSetting).join(SiteDER).where(SiteDER.site_id == site.site_id).limit(1)
-    )
+    response = await session.execute(select(SiteDERSetting).where(SiteDERSetting.site_id == site.site_id).limit(1))
     der_settings = response.scalar_one_or_none()
     if der_settings is None:
         return CheckResult(False, f"No DERSetting found for EndDevice {site.site_id}.")
@@ -456,9 +453,7 @@ async def check_der_capability_contents(
     if site is None:
         return CheckResult(False, "No EndDevice is currently registered.")
 
-    response = await session.execute(
-        select(SiteDERRating).join(SiteDER).where(SiteDER.site_id == site.site_id).limit(1)
-    )
+    response = await session.execute(select(SiteDERRating).where(SiteDERRating.site_id == site.site_id).limit(1))
     der_rating = response.scalar_one_or_none()
     if der_rating is None:
         return CheckResult(False, f"No DERCapability found for EndDevice {site.site_id}.")
@@ -514,9 +509,7 @@ async def check_der_status_contents(session: AsyncSession, resolved_parameters: 
     if site is None:
         return CheckResult(False, "No EndDevice is currently registered.")
 
-    response = await session.execute(
-        select(SiteDERStatus).join(SiteDER).where(SiteDER.site_id == site.site_id).limit(1)
-    )
+    response = await session.execute(select(SiteDERStatus).where(SiteDERStatus.site_id == site.site_id).limit(1))
     der_status = response.scalar_one_or_none()
     if der_status is None:
         return CheckResult(False, f"No DERStatus found for EndDevice {site.site_id}.")
