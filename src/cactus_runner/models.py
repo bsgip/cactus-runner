40939050e580c6ab9e46cb4277356c5b984c80c7
Lachlan <161661313+LachlanJW@users.noreply.github.com>
2026-08-05 13:28:25 +1000

    Added warnings to test run (#202)
    * schema
    
    * wip
    
    * wip
    
    * wip
    
    * test
    
    * one query not 3
    
    * test
    
    * sync schema
    
    * wip
    
    * reading type warning
    
    * test

--- src/cactus_runner/models.py
+++ src/cactus_runner/models.py
@@ -10,6 +10,7 @@ from cactus_schema.runner import (
     ClientInteractionType,
     RequestEntry,
     StepStatus,
+    WarningEntry,
 )
 from cactus_test_definitions import CSIPAusVersion
 from cactus_test_definitions.client import Event, TestProcedure
@@ -136,6 +137,7 @@ class ActiveTestProcedure:
     resource_annotations: ResourceAnnotations = field(default_factory=ResourceAnnotations)
     random_values: RandomValues = field(default_factory=RandomValues)
     proxy_route_overrides: list[ProxyRouteOverride] = field(default_factory=list)
+    warnings: dict[str, WarningEntry] = field(default_factory=dict)
 
     def is_finished(self) -> bool:
         """True if the active test procedure has been marked as finished. That is, there is no more test data to
@@ -713,4 +715,4 @@ class ReportingData_v1(ReportingData_Base):  # noqa: N801
     readings: list[PackedReadings]
     sites: list[Site]
     timeline: Timeline | None
-    set_max_w_varied: bool = False
+    warnings: list[WarningEntry] = field(default_factory=list)
